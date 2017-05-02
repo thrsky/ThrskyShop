29a01350f1aa9bc1d8e071e7b1a15ee55e5c7850
thrsky <1491474343@qq.com>
2017-05-02 08:56:47 +0800

    guava缓存使用

---
+++ src/main/java/com/thrsky/shop/common/TokenCache.java
@@ -0,0 +1,49 @@
+package com.thrsky.shop.common;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ *  Guava 缓存使用
+ * Created by thRShy on 2017/5/2.
+ */
+public class TokenCache {
+
+    private static Logger logger = LoggerFactory.getLogger(TokenCache.class);
+
+    private static LoadingCache<String,String> loadCache= CacheBuilder.newBuilder().initialCapacity(1000).maximumSize(10000).expireAfterAccess(12, TimeUnit.HOURS)
+            .build(new CacheLoader<String, String>() {
+                //默认的数据加载实现  当调用get取值的时候，如果key没有对应的值，就调用这个方法
+                @Override
+                public String load(String s) throws Exception {
+                    return "null";
+                }
+            });
+
+    public static void setKey(String key,String value){
+        loadCache.put(key,value);
+    }
+
+    public static String getKey(String key){
+        String value=null;
+        try {
+            value=loadCache.get(key);
+            if("null".equals(value)){
+                return null;
+            }
+            return value;
+        } catch (ExecutionException e) {
+            logger.error("localCache get error",e);
+//            e.printStackTrace();
+        }
+    return null;
+    }
+
+}
