f7d597118259be30db67756d20df518668e1f635
thrsky <1491474343@qq.com>
2017-05-09 16:42:32 +0800

    定义管理员和用户的身份标识

--- src/main/java/com/thrsky/shop/common/Const.java
+++ src/main/java/com/thrsky/shop/common/Const.java
@@ -7,4 +7,13 @@ package com.thrsky.shop.common;
 public class Const {
 
     public static final String CURRENT_USER="current_user";
+
+    public static final String EMAIL="email";
+
+    public static final String USERNAME="username";
+
+    public interface Role{
+        int ROLE_CUSTOMER = 0;
+        int ROLE_ADMIN=1;
+    }
 }
