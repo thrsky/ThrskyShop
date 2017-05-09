0d023898adefd65b34e2713492ff9a004bee15e4
thrsky <1491474343@qq.com>
2017-05-09 16:44:55 +0800

    用户service 接口

--- src/main/java/com/thrsky/shop/service/IUserService.java
+++ src/main/java/com/thrsky/shop/service/IUserService.java
@@ -9,4 +9,12 @@ import com.thrsky.shop.pojo.User;
 public interface IUserService {
 
     ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str,String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username,String answer,String question);
 }
