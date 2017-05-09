4418fd6657758459f23f767cfe88d08578905a6f
thrsky <1491474343@qq.com>
2017-05-09 16:43:56 +0800

    update

--- src/main/java/com/thrsky/shop/common/LoginEnum.java
+++ src/main/java/com/thrsky/shop/common/LoginEnum.java
@@ -1,11 +1,13 @@
 package com.thrsky.shop.common;
 
 /**
+ *  登录时可能会出现的 枚举类
  * Created by thRShy on 2017/5/1.
  */
 public enum LoginEnum {
     NO_USER(3,"用户不存在"),
     ERROR_PASSWORD(-1,"密码错误"),
+    USER_NO_LOGIN(-2,"用户未登录"),
     LOGIN_SUCCESS(1,"登录成功"),
     ;
 
